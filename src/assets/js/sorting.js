2f36de4dfd8c2b35f78a73e3bff9049ce9081fd8
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-06-22 16:39:38 +0200

    fix sorting

--- src/assets/js/sorting.js
+++ src/assets/js/sorting.js
@@ -4,6 +4,14 @@ $(function() {
 
     $( "#sortable" ).sortable({
         axis: 'y',
+        placeholder: "ui-state-highlight",
+        start: function (event, ui) {
+            ui.item.css("height","100px");
+            ui.item.css("overflow","hidden");
+        },
+        stop: function (event, ui) {
+            //ui.item.toggleClass("highlight");
+        },
         update: function (event, ui) {
             var data = $(this).sortable('serialize');
             var data = $(this).sortable('serialize') +"&_token=" + $("meta[name='_token']").attr('content');
