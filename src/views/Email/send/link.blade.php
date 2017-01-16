c3bad3832e68bd99eba75e389adf66ce424a3101
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-01-16 10:53:47 +0100

    header link class

--- src/views/Email/send/link.blade.php
+++ src/views/Email/send/link.blade.php
@@ -5,7 +5,7 @@
         <table border="0" width="600" align="center" cellpadding="0" cellspacing="0" class="tableReset">
             <tr bgcolor="f5f5f5" align="center" class="resetMarge" style="height: 30px;line-height: 30px;display: block;">
                 <td class="resetMarge">
-                    <a style="text-decoration: none; font-weight: bold; color: #000; height: 30px;line-height: 30px; display: block;" href="{{ url('/pdf/'.$campagne->id) }}" target="_blank">
+                    <a class="link_pdf" style="text-decoration: none; font-weight: bold; color: #000; height: 30px;line-height: 30px; display: block;" href="{{ url('/pdf/'.$campagne->id) }}" target="_blank">
                         Télécharger en pdf
                     </a>
                 </td>
