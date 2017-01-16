82ec744a2f572d9c2718a0ed118c52773d15c5c5
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-01-16 09:17:23 +0100

    print styles header2

--- src/views/Frontend/pdf.blade.php
+++ src/views/Frontend/pdf.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 
     <div class="header">
-        <h1 style="font-size: 21px; margin: 5px 0 5px 0; line-height: 20px;">Newsletter {{ config('app.name') }}</h1>
-        <h2 style="font-size: 18px; margin: 0 0 5px 0; line-height: 20px;">{{ $campagne->sujet }}</h2>
-        <h3 style="font-size: 15px; margin: 0; line-height: 20px;">{{ $campagne->auteurs }}</h3>
+        <h1>Newsletter {{ config('app.name') }}</h1>
+        <h2>{{ $campagne->sujet }}</h2>
+        <h3>{{ $campagne->auteurs }}</h3>
     </div>
 
     @if(!$campagne->content->isEmpty())
